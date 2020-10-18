90669139df34d898983eebab3b613c445bc3a983
llaryssa <llaryssa@users.noreply.github.com>
2020-10-18 14:49:46 +0000

    Automated deployment: Sun Oct 18 14:49:46 UTC 2020 a7088ced5da26fdb9932ec78357bbe240f0a8642

--- src/components/Icon/index.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "preact";
-import { Image } from "preact-fluid";
-
-export default class Icon extends Component {
-  render({ src }) {
-    return <Image className="image-icon" src={src} />;
-  }
-}
