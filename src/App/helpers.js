880f15db91e84bbfa0555b0c342b6d8bd8b131e3
llaryssa <llaryssa@me.com>
2020-10-18 21:20:03 -0300

    refactors app component

---
+++ src/App/helpers.js
@@ -0,0 +1,40 @@
+var kgToLbRatio = 2.20462
+
+var getBarbellWeight = (type, unit) => {
+  if (unit === 'lb') return type === 'female' ? 35 : 45
+  if (unit === 'kg') return type === 'female' ? 15 : 20
+}
+
+var convertWeight = (weight, inputUnit, outputUnit) => {
+  if (inputUnit === 'kg' && outputUnit === 'lb') return weight * kgToLbRatio
+  if (inputUnit === 'lb' && outputUnit === 'kg') return weight / kgToLbRatio
+}
+
+var calculateResults = (
+  weightString,
+  percentageString,
+  barbellType,
+  inputUnit,
+  outputUnit
+) => {
+  var weight = parseFloat(weightString)
+  var percentage = parseFloat(percentageString)
+  var barbellWeight = getBarbellWeight(barbellType, outputUnit)
+  var willConvert = inputUnit != outputUnit
+  var willApplyPercentage = percentage !== 100
+
+  var convertedWeight = willConvert
+    ? convertWeight(weight, inputUnit, outputUnit)
+    : weight
+  var finalWeight = willApplyPercentage
+    ? (percentage * convertedWeight) / 100
+    : convertedWeight
+
+  return {
+    converted: willConvert ? convertedWeight : undefined,
+    final: finalWeight,
+    eachSide: (finalWeight - barbellWeight) / 2
+  }
+}
+
+module.exports = { calculateResults }
